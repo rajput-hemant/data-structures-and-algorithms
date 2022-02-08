38392a287c822eea7f6426046be3315922ecf9ea
Hemant Rajput <rajput.hemant2001@gmail.com>
2022-02-09 00:43:48 +0530

    Created FloydsAlgorithm.java

--- dsa/linkedlist/FloydsAlgorithm.java
+++ dsa/linkedlist/FloydsAlgorithm.java
@@ -1,17 +1,43 @@
 package linkedlist;
 
 public class FloydsAlgorithm {
-    // Node head;
+    static Node head;
 
-    class Node {
+    static class Node {
+        int data;
         Node next = null;
 
-        Node(int next) {
+        Node(int data) {
+            this.data = data;
             this.next = null;
         }
     }
 
-    // Tortoise-Hare-Approach (Floyd's Algorithm)
+    public static void main(String[] args) {
+        FloydsAlgorithm list = new FloydsAlgorithm();
+        list.head = new Node(50);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(15);
+        list.head.next.next.next = new Node(4);
+        list.head.next.next.next.next = new Node(10);
+        // Creating a loop for testing
+        head.next.next.next.next.next = head.next.next;
+        // list.printList(head);
+        list.removeCycle(head);
+        System.out.print("LinkedList after removing loop -> [");
+        list.printList(head);
+        System.out.println("\b\b]");
+    }
+
+    public void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + ", ");
+            head = head.next;
+        }
+    }
+
+    // Tortoise-Hare-Approach (Floyd's Algorithm) to Find the Middle Node in the
+    // LinkedList
     // The Hare pointer leaps 2 elements while turtle pointer leaps one
     public static Node findMiddle(Node head) {
         Node hare = head, turtle = head;
@@ -27,7 +53,9 @@ public class FloydsAlgorithm {
         return turtle;
     }
 
+    // Fn to check if List has loop/cycle
     public boolean hasCycle(Node head) {
+        // If list is empty
         if (head == null)
             return false;
         Node fast = head, slow = head;
@@ -35,8 +63,37 @@ public class FloydsAlgorithm {
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow)
-                return false;
+                return true;
         }
-        return true;
+        return false;
+    }
+
+    // Fn to remove loop/cycle from the List if any
+    public void removeCycle(Node head) {
+        Node slow = head, fast = head;
+        boolean isCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                isCycle = true;
+                break;
+            }
+        }
+        // If there is loop/cycle
+        if (isCycle) {
+            // slow pointer is set to head of the list, while fast is still on the node where fast & slow pointer met
+            slow = head;
+            // both pointers are traversed with one leap, the meeting node will be the starting node of the loop
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            // fast pointer is traversed with on leap from meeting node to the end of the list
+            while (fast.next != slow)
+                fast = fast.next;
+            // when the last node is found, the next of last node is set to null to remove the loop/cycle
+            fast.next = null;
+        }
     }
 }
