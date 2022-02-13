4fdaee75aefd4eeddb4bbbaba15e3ccc3c1f914d
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-02-13 21:09:13 +0530

    updated SumOfNodes.java

--- dsa/tree/BinaryTree/SumOfNodes.java
+++ dsa/tree/BinaryTree/SumOfNodes.java
@@ -5,17 +5,20 @@ import tree.BinaryTree.BinaryTreeBuilder.Node;
 public class SumOfNodes {
     public static void main(String[] args) {
         // int[] nodes = BinaryTreeHelper.getInput();
-        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Node root = BinaryTreeBuilder.buildTree(nodes);
         System.out.println("Sum Of Nodes -> " + sumOfNodes(root));
     }
 
+    // Fn to print the sum of the nodes
     public static int sumOfNodes(Node root) {
         // base case
+        // if node is null
         if (root == null)
             return 0;
         int leftSum = sumOfNodes(root.left);
         int rightSum = sumOfNodes(root.right);
+        // through recursive call, the sum of leftChild, rightChild & parentNode is returned to the upperNode, till rootNode
         return leftSum + rightSum + root.data;
     }
 }
