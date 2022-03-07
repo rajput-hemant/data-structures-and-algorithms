d9319cffc59a68bc90393aa002380b2e49b5c3fa
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-03-07 19:20:54 +0530

    Created factorial

---
+++ DsaWithCpp/Recursion/Factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main()
+{
+    int n;
+    cout << "Enter the Number -> ";
+    cin >> n;
+    printf("Factorial of '%d' is -> '%d'\n", n, factorial(n));
+    return 0;
+};
+
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+};
