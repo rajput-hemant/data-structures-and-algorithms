2c208b0d58c337e198002e7101e3820fbb69f614
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-03-21 16:52:52 +0530

    Definition Of Node
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ dsaWithJava/tree/binarytree/Node.java
@@ -0,0 +1,22 @@
+package tree.binarytree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node() {
+
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+
+    Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
