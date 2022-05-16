1b19a8ce25d6035f8e5dd28e510c882343e5fe24
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-05-16 13:48:48 +0530

    Updated PriorityQueue.java: added JavaDoc comments
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- dsaWithJava/heap/PriorityQueue.java
+++ dsaWithJava/heap/PriorityQueue.java
@@ -5,36 +5,79 @@ import java.util.NoSuchElementException;
 public class PriorityQueue {
     MinHeap mh = new MinHeap();
 
+    /**
+     * Returns {@code true} if Priority Queue contains no elements.
+     *
+     * @return {@code true} if Priority Queue contains no elements
+     */
     public boolean isEmpty() {
         return mh.isEmpty();
     }
 
+    /**
+     * Returns the number of elements in the Priority Queue
+     * 
+     * @return the number of elements in the Priority Queue
+     */
     public int size() {
         return mh.size();
     }
 
+    /**
+     * Inserts the element to the Priority Queue
+     * 
+     * @param item element to add
+     */
     public void add(int item) {
         mh.insert(item);
     }
 
+    /**
+     * Retrieves, but does not remove, the head of this Priority Queue, or returns
+     * {@code null} if it is empty.
+     * 
+     * @return the head of this Priority Queue, or {@code null} if it is empty
+     */
     public Integer peek() {
         if (isEmpty())
             return null;
         return mh.getMin();
     }
 
+    /**
+     * Retrieves, but does not remove, the head of the Priority Queue. This method
+     * differs from {@link #peek peek} only in that it throws an exception
+     * if the Priority Queue is empty.
+     *
+     * @return the head of the Priority Queue
+     * @throws NoSuchElementException if the Priority Queue is empty
+     */
     public Integer element() {
         if (isEmpty())
             throw new NoSuchElementException();
         return mh.getMin();
     }
 
+    /**
+     * Retrieves and removes the head of the Priority Queue, or returns {@code null}
+     * if it is empty.
+     *
+     * @return the head of the Priority Queue, or {@code null} if its is empty
+     */
     public Integer poll() {
         if (isEmpty())
             return null;
         return mh.extractMin();
     }
 
+    /**
+     * Retrieves and removes the head of the Priority Queue. This method differs
+     * from {@link #poll() poll()} only in that it throws an exception if
+     * the Priority Queue is empty.
+     *
+     * @return the head of the Priority Queue
+     * @throws NoSuchElementException if the Priority Queue is empty
+     */
     public int remove() {
         if (isEmpty())
             throw new NoSuchElementException();
